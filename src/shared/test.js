ab77b822db6162b85c1eed7b7b12186bd1285235
Will Carpenter <willcarpenteriii@gmail.com>
2017-11-14 15:29:35 -0700

    added webpack build

---
+++ src/shared/test.js
@@ -0,0 +1,5 @@
+export function addNums(a, b) {
+	console.log(a + b)
+
+	return a + b
+}
---
+++ webpack.server.js
@@ -0,0 +1,27 @@
+/* eslint-disable object-curly-newline */
+const path = require('path')
+
+function createConfig(isDebug) {
+	return {
+		target:  "node",
+		devtool: "source-map",
+		entry: './src/server/server.js',
+		output:  {
+			path:     path.join(__dirname, "build"),
+			filename: "server.js"
+		},
+		resolve: {
+			alias: {
+				shared: path.join(__dirname, "src", "shared")
+			}
+		},
+		module:  {
+			loaders: [
+				{ test: /\.js$/, loader: "babel-loader", exclude: /node_modules/ },
+				{ test: /\.js$/, loader: "eslint-loader", exclude: /node_modules/ }
+			]
+		}
+	};
+}
+
+module.exports = createConfig(true)
